402469a2fb701c2fa519c47906e6a652722a1ce4
Bill Gatte <mbillgatte@gmail.com>
2026-05-12 17:36:15 -0500

    Je fais les exercice du chapitre 1

--- lib/screens_bill/chapitre1.dart
+++ lib/screens_bill/chapitre1.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:untitled1/screens_bill/chapitre1ExoBill/exercice1Bill.dart';
+import 'package:untitled1/screens_bill/chapitre1ExoBill/exercice2Bill.dart';
 
 class Chapitre1Screen extends StatelessWidget {
   const Chapitre1Screen({super.key});
@@ -29,6 +30,23 @@ class Chapitre1Screen extends StatelessWidget {
           },
         ),
           const Divider(),
+
+          //Exo 2
+          ListTile(
+            leading: const Icon(Icons.ice_skating, size: 30,),
+            title: const Text(
+              'Exercice 2:',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            subtitle: const Text('Voir lexercice 2  proposer par bill'),
+            trailing: const Icon(Icons.arrow_forward),
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder:(context) => const Exercice2Screen()));
+            },
+          ),
+          const Divider(),
+
 
 
 
---
+++ lib/screens_bill/chapitre1ExoBill/exercice2Bill.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Exercice2Screen extends StatelessWidget {
+  const Exercice2Screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Exercice 2")),
+      body: const Center(child: Text("Contenu de l'exercice 2")),
+    );
+  }
+}
